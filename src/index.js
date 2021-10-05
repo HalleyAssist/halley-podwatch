6b918b90cad566d949dd93b43bb08982b63c970a
Mathew Heard <splitice@users.noreply.github.com>
2021-10-05 15:39:34 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = {
+    /* monitoring modules */
     ExpressMonitor: require('./modules/ExpressMonitor'),
     NodeJsMonitor: require('./modules/NodeJsMonitor'),
     KnexMonitor: require('./modules/KnexMonitor'),
-    SyncFunctionMonitor: require('./modules/SyncFunctionMonitor')
+    SyncFunctionMonitor: require('./modules/SyncFunctionMonitor'),
+
+    /* helpers */
+    ZabbixSender: require('./helpers/ZabbixSender')
 }
